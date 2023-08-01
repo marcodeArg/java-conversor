0819c9b1ecf06911ed8721688fc09cf66a97fc0c
mrc <marcoemorales2015@gmail.com>
2023-07-31 23:58:42 -0300

    Agregada clase monedas y metodo para obtener las descripciones

---
+++ ConversorMonedas/src/logica/Monedas.java
@@ -0,0 +1,25 @@
+package logica;
+
+public class Monedas {
+	private String symbol;
+	private String description;
+	
+	
+	public String getSymbol() {
+		return symbol;
+	}
+	
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
